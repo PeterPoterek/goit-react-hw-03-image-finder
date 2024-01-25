53b6f0d669dc4394302288cdccf8d8c4a21d3b1e
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-25 18:15:07 +0100

    Searchbar styled componenets

---
+++ src/components/Searchbar/SearchbarStyles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const SearchbarHeader = styled.header``;
+export const SearchbarForm = styled.form``;
+export const SearchbarSubmitButton = styled.button``;
+export const SearchbarSubmitButtonText = styled.span``;
+export const SearchbarInput = styled.input``;
