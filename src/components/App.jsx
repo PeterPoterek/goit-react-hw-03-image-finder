0f0ad26605972e98a35c3a5bebaabbc2f98e5243
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-25 19:04:12 +0100

    fetch pixabay API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Searchbar from './Searchbar/Searchbar.jsx';
 import ImageGallery from './ImageGallery/ImageGallery.jsx';
 
 class App extends Component {
   apiKey = '41114633-51106070bf303d1c44ed5d4b9';
+  url = 'https://pixabay.com/api/';
 
   constructor() {
     super();
@@ -13,14 +15,33 @@ class App extends Component {
       currentSearchInput: '',
     };
   }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.currentSearchInput !== this.state.currentSearchInput) {
+      console.log(this.state.currentSearchInput);
+
+      axios
+        .get(this.url, {
+          params: {
+            key: this.apiKey,
+            q: this.state.currentSearchInput,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            page: 1,
+            per_page: 12,
+          },
+        })
+        .then(res => {
+          console.log(res.data);
+        });
+    }
+  }
 
   handleImageSearch = e => {
     e.preventDefault();
 
     const userSearchInput = e.target[1].value;
     this.setState({ currentSearchInput: userSearchInput });
-
-    console.log(this.state.currentSearchInput);
   };
   render() {
     return (
