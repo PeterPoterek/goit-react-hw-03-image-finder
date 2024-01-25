335790670d74a112d686e4ed92d1c166fc69ab01
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-25 21:19:23 +0100

    renderMoreImages refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,7 @@ class App extends Component {
     currentSearchInput: '',
     imagesToRender: [],
     currentPage: 1,
+    totalHits: 0,
   };
 
   handleImageSearch = e => {
@@ -40,45 +41,49 @@ class App extends Component {
           this.setState({
             imagesToRender: res.data.hits,
             currentPage: 1,
+            totalHits: res.data.totalHits,
           });
         });
     }
   }
 
   renderMoreImages = () => {
-    const { currentSearchInput, currentPage, imagesToRender } = this.state;
+    const { currentSearchInput, currentPage, imagesToRender, totalHits } =
+      this.state;
+    const imagesPerPage = 12;
 
-    axios
-      .get(this.url, {
-        params: {
-          key: this.apiKey,
-          q: currentSearchInput,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          page: currentPage + 1,
-          per_page: 12,
-        },
-      })
-      .then(res => {
-        const newImages = res.data.hits;
-        const updatedImages = [...imagesToRender, ...newImages];
+    if (currentPage * imagesPerPage < totalHits) {
+      axios
+        .get(this.url, {
+          params: {
+            key: this.apiKey,
+            q: currentSearchInput,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            page: currentPage + 1,
+            per_page: 12,
+          },
+        })
+        .then(res => {
+          const newImages = res.data.hits;
+          const updatedImages = [...imagesToRender, ...newImages];
 
-        this.setState({
-          imagesToRender: updatedImages,
-          currentPage: currentPage + 1,
+          this.setState({
+            imagesToRender: updatedImages,
+            currentPage: currentPage + 1,
+          });
         });
-      });
+    }
   };
-
   render() {
-    const { imagesToRender } = this.state;
+    const { imagesToRender, totalHits } = this.state;
 
     return (
       <>
         <Searchbar handleImageSearch={this.handleImageSearch} />
         <ImageGallery imagesToRender={imagesToRender} />
-        {imagesToRender.length > 0 && (
+        {imagesToRender.length > 0 && imagesToRender.length < totalHits && (
           <Button renderMoreImages={this.renderMoreImages} />
         )}
       </>
