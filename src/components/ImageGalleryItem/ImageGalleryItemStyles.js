b943548acc1ea6b2a1b6815774f605441d5e943d
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-26 17:44:32 +0100

    image hover anim

--- src/components/ImageGalleryItem/ImageGalleryItemStyles.js
+++ src/components/ImageGalleryItem/ImageGalleryItemStyles.js
@@ -4,6 +4,14 @@ export const ImageGalleryLi = styled.li`
   border-radius: 2px;
 
   border: solid 5px #262626;
+
+  cursor: pointer;
+
+  transition: transform 0.1s ease-in-out;
+
+  &:hover {
+    transform: scale(1.03);
+  }
 `;
 export const ImageGalleryImage = styled.img`
   width: 100%;
