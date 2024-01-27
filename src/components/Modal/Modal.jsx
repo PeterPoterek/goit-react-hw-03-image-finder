3b832dd40137e81387d37e75210b9d9e88aeccb4
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-27 16:07:32 +0100

    close modal on esc

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,33 @@
+import { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import { ModalContainer, ModalImage, ModalCloseButton } from './ModalStyles';
 
-const Modal = ({ closeModal, largeImageURL }) => {
-  return (
-    <ModalContainer>
-      <ModalImage src={largeImageURL}></ModalImage>
-      <ModalCloseButton onClick={closeModal}>X</ModalCloseButton>
-    </ModalContainer>
-  );
-};
+class Modal extends Component {
+  handleKeyPress = event => {
+    if (event.key === 'Escape' || event.key === 'Esc') {
+      this.props.closeModal();
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  render() {
+    const { closeModal, largeImageURL } = this.props;
+
+    return (
+      <ModalContainer>
+        <ModalImage src={largeImageURL}></ModalImage>
+        <ModalCloseButton onClick={closeModal}>X</ModalCloseButton>
+      </ModalContainer>
+    );
+  }
+}
 
 Modal.propTypes = {
   closeModal: PropTypes.func.isRequired,
